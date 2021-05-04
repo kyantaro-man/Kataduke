f13c6a5eb6415af6210b65370a066e5d36e8a24b
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2021-05-04 18:16:47 +0900

    Routingの設定

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('rooms/{id}/items', 'ItemsController@index')->name('items.index');

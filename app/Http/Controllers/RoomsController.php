c77e07fa40d592578be26b52aeaffad558121ef8
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2021-05-04 23:06:02 +0900

    コントローラー設定

---
+++ app/Http/Controllers/RoomsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Room;
+
+class RoomsController extends Controller
+{
+    // ルームの登録フォームを表示する
+    public function showCreateForm() {
+        return view('rooms/create');
+    }
+
+    // ルームを登録する
+    public function create(Request $request) {
+        $room = new Room();
+
+        $room->name = $request->name;
+        $room->size = $request->size;
+
+        $room->save();
+
+        return redirect()->route('items.index', [
+            'id' => $room->id,
+        ]);
+    }
+}

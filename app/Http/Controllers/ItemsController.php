0677f61caf66ee379f63918675b391d9d70cd24d
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2021-05-04 19:55:41 +0900

    コントローラーの設定

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Room;
+
+class ItemsController extends Controller
+{
+    // アイテム一覧を表示する
+    public function index(int $id)
+    {
+        $rooms = Room::all();
+
+        return view('items/index', [
+            'rooms' => $rooms,
+            'current_folder_id' => $id,
+        ]);
+    }
+}

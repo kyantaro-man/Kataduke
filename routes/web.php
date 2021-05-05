44a553bc8c55d1cc4e27ab88abe91b70d62a202e
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2021-05-05 16:45:10 +0900

    Routingの設定(ログイン時のみ)

--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/rooms/{id}/items', 'App\Http\Controllers\ItemsController@index')->name('items.index');
+Route::group(['middleware' => 'auth'], function() {
+  Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/rooms/create', 'App\Http\Controllers\RoomsController@showCreateForm')->name('rooms.create');
-Route::post('/rooms/create', 'App\Http\Controllers\RoomsController@create');
+  Route::get('/rooms/{id}/items', 'App\Http\Controllers\ItemsController@index')->name('items.index');
 
-Route::get('/rooms/{id}/items/create', 'App\Http\Controllers\ItemsController@showCreateForm')->name('items.create');
-Route::post('/rooms/{id}/items/create', 'App\Http\Controllers\ItemsController@create');
+  Route::get('/rooms/{id}/items/create', 'App\Http\Controllers\ItemsController@showCreateForm')->name('items.create');
+  Route::post('/rooms/{id}/items/create', 'App\Http\Controllers\ItemsController@create');
 
-Route::get('/rooms/{id}/edit', 'App\Http\Controllers\RoomsController@showEditForm')->name('rooms.edit');
-Route::post('/rooms/{id}/edit', 'App\Http\Controllers\RoomsController@edit');
+  Route::get('/rooms/{id}/items/{item_id}/edit', 'App\Http\Controllers\ItemsController@showEditForm')->name('items.edit');
+  Route::post('/rooms/{id}/items/{item_id}/edit', 'App\Http\Controllers\ItemsController@edit');
 
-Route::get('/rooms/{id}/items/{item_id}/edit', 'App\Http\Controllers\ItemsController@showEditForm')->name('items.edit');
-Route::post('/rooms/{id}/items/{item_id}/edit', 'App\Http\Controllers\ItemsController@edit');
+  Route::get('/rooms/create', 'App\Http\Controllers\RoomsController@showCreateForm')->name('rooms.create');
+  Route::post('/rooms/create', 'App\Http\Controllers\RoomsController@create');
+
+  Route::get('/rooms/{id}/edit', 'App\Http\Controllers\RoomsController@showEditForm')->name('rooms.edit');
+  Route::post('/rooms/{id}/edit', 'App\Http\Controllers\RoomsController@edit');
+});
 
 Auth::routes();
-
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

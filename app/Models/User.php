19e44b03e5dc71eb26e44c09ca7a133c42e83096
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2021-05-05 16:00:45 +0900

    モデルの設定

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function rooms() {
+        return $this->hasMany('App\Models\Room');
+    }
 }

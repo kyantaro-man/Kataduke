9aa53e66026f3afdff5b26817718275f3191d477
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2021-05-05 18:41:30 +0900

    ルートモデルバインディング化

--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => 'auth'], function() {
   Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-  Route::get('/rooms/{id}/items', 'App\Http\Controllers\ItemsController@index')->name('items.index');
+  Route::get('/rooms/{room}/items', 'App\Http\Controllers\ItemsController@index')->name('items.index');
 
-  Route::get('/rooms/{id}/items/create', 'App\Http\Controllers\ItemsController@showCreateForm')->name('items.create');
-  Route::post('/rooms/{id}/items/create', 'App\Http\Controllers\ItemsController@create');
+  Route::get('/rooms/{room}/items/create', 'App\Http\Controllers\ItemsController@showCreateForm')->name('items.create');
+  Route::post('/rooms/{room}/items/create', 'App\Http\Controllers\ItemsController@create');
 
-  Route::get('/rooms/{id}/items/{item_id}/edit', 'App\Http\Controllers\ItemsController@showEditForm')->name('items.edit');
-  Route::post('/rooms/{id}/items/{item_id}/edit', 'App\Http\Controllers\ItemsController@edit');
+  Route::get('/rooms/{room}/items/{item}/edit', 'App\Http\Controllers\ItemsController@showEditForm')->name('items.edit');
+  Route::post('/rooms/{room}/items/{item}/edit', 'App\Http\Controllers\ItemsController@edit');
 
   Route::get('/rooms/create', 'App\Http\Controllers\RoomsController@showCreateForm')->name('rooms.create');
   Route::post('/rooms/create', 'App\Http\Controllers\RoomsController@create');
 
-  Route::get('/rooms/{id}/edit', 'App\Http\Controllers\RoomsController@showEditForm')->name('rooms.edit');
-  Route::post('/rooms/{id}/edit', 'App\Http\Controllers\RoomsController@edit');
+  Route::get('/rooms/{room}/edit', 'App\Http\Controllers\RoomsController@showEditForm')->name('rooms.edit');
+  Route::post('/rooms/{room}/edit', 'App\Http\Controllers\RoomsController@edit');
 });
 
 Auth::routes();

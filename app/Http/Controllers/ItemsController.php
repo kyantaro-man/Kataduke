8f22987e064ce36d60ca3199e47d1bc0c22a2faa
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2021-05-05 16:59:26 +0900

    コントローラーの設定(リレーション)

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -7,13 +7,14 @@ use App\Models\Room;
 use App\Models\Item;
 use App\Http\Requests\CreateItem;
 use App\Http\Requests\EditItem;
+use Illuminate\Support\Facades\Auth;
 
 class ItemsController extends Controller
 {
     // アイテム一覧を表示する
     public function index(int $id)
     {
-        $rooms = Room::all();
+        $rooms = Auth::user()->rooms()->get();
 
         $current_room = Room::find($id);
 
